37af3ae28a83ba3a0e55140587610e686f64926d
Neil Cummings <trycatchlearn@outlook.com>
2024-07-13 14:07:51 +0700

    End of section 10

---
+++ client/src/app/features/shop/product-details/product-details.component.ts
@@ -0,0 +1,44 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { ShopService } from '../../../core/services/shop.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../../../shared/models/product';
+import { CurrencyPipe } from '@angular/common';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { MatDivider } from '@angular/material/divider';
+
+@Component({
+  selector: 'app-product-details',
+  standalone: true,
+  imports: [
+    CurrencyPipe,
+    MatButton,
+    MatIcon,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatDivider
+  ],
+  templateUrl: './product-details.component.html',
+  styleUrl: './product-details.component.scss'
+})
+export class ProductDetailsComponent implements OnInit {
+  private shopService = inject(ShopService);
+  private activatedRoute = inject(ActivatedRoute);
+  product?: Product;
+
+  ngOnInit(): void {
+    this.loadProduct();
+  }
+
+  loadProduct() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    if (!id) return;
+    this.shopService.getProduct(+id).subscribe({
+      next: product => this.product = product,
+      error: error => console.log(error)
+    })
+  }
+}
--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatBadge } from '@angular/material/badge';
 import { MatButton } from '@angular/material/button';
 import {MatIcon} from '@angular/material/icon';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -9,7 +10,9 @@ import {MatIcon} from '@angular/material/icon';
   imports: [
     MatIcon,
     MatButton,
-    MatBadge
+    MatBadge,
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
